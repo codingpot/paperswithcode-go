63608c9c20fea5a21c3da8346a17cf11a219e391
Mo Kweon <kkweon@gmail.com>
2021-05-28 21:40:51 -0700

    Remove sendRequest function

--- client.go
+++ client.go
@@ -1,9 +1,6 @@
 package paperswithcode_go
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
 	"github.com/codingpot/paperswithcode-go/internal/transport"
 	"net/http"
 	"time"
@@ -44,38 +41,4 @@ type Client struct {
 	BaseURL    string
 	HTTPClient *http.Client
 	apiToken   string
-}
-
-type errorResponse struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-}
-
-func (c *Client) sendRequest(req *http.Request, v interface{}) error {
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-	req.Header.Set("Accept", "application/json; charset=utf-8")
-
-	res, err := c.HTTPClient.Do(req)
-	if err != nil {
-		return err
-	}
-
-	defer res.Body.Close()
-
-	if res.StatusCode < http.StatusOK || res.StatusCode >= http.StatusBadRequest {
-		var errRes errorResponse
-		if err = json.NewDecoder(res.Body).Decode(&errRes); err == nil {
-			return errors.New(errRes.Message)
-		}
-
-		return fmt.Errorf("unknown error, status code: %d", res.StatusCode)
-	}
-
-	if err = json.NewDecoder(res.Body).Decode(&v); err != nil {
-		return err
-	}
-
-	// fmt.Println(fullResponse)
-
-	return nil
 }
